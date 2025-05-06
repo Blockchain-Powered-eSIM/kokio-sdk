f0c920f60b80917fe3a6346b5d138ffb77d8e655
ManulParihar <manul.singh.parihar@gmail.com>
2025-05-06 18:44:24 +0530

    Fixed bug in index calculation

--- src/logic/services/turnkeyClient.ts
+++ src/logic/services/turnkeyClient.ts
@@ -145,17 +145,17 @@ export const _stampAndSignMessageWithTurnkey = async (client: TurnkeyClient, org
     const authenticatorData = isoBase64URL.toBuffer(stampedHeaderValue.authenticatorData);
     console.log("SDK authenticatorData: ", toHex(authenticatorData));
     
-    const clientDataJSON = stampedHeaderValue.clientDataJSON;
-    const typeIndex = clientDataJSON.indexOf('"type":');
+    const decodedClientDataJson = decodeClientDataJSON(stampedHeaderValue.clientDataJson);
+    console.log("SDK decodedClientDataJson: ", JSON.stringify(decodedClientDataJson, null, 2));
+    
+    const typeIndex = JSON.stringify(decodedClientDataJson).indexOf('"type":"webauthn.get"');
     console.log("SDK typeIndex: ", typeIndex);
-    const challengeIndex = clientDataJSON.indexOf('"challenge":');
+    const challengeIndex = JSON.stringify(decodedClientDataJson).indexOf('"challenge":');
     console.log("challengeIndex: ", challengeIndex);
     if (typeIndex === -1 || challengeIndex === -1) {
         throw new Error("Could not find type or challenge in clientDataJSON");
     }
-    
-    const decodedClientDataJson = decodeClientDataJSON(stampedHeaderValue.clientDataJson);
-    console.log("SDK decodedClientDataJson: ", JSON.stringify(decodedClientDataJson, null, 2));
+
     const signature = assertNonNull(response.activity.result.signRawPayloadResult);
     console.log("SDK signature: ", JSON.stringify(signature, null, 2));
     const webAuthnSignature = {
